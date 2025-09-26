25254f5ce2e512fa8ba8c59243276e813a1d213f
itsmanasdash <mdash3735@gmail.com>
2025-09-26 19:02:19 +0530

    fixed cors error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "16kb"})); // to prevent large payload attacks
 app.use(cors({
   credentials : true,
-  origin : "http://localhost:5173" // my frontend url
+  origin : "https://sweet-shop-lac.vercel.app" // my frontend url
 }));
 
 app.use("/api/auth", authRouter);
